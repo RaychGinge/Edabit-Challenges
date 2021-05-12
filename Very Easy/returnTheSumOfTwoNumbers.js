8bb1265a7d25105e07eeb30196114745784e733d
RaychGinge <60284593+RaychGinge@users.noreply.github.com>
2021-05-12 22:11:43 +0100

    Create returnTheSumOfTwoNumbers.js
    create a function that takes two numbers and return their sum

---
+++ Very Easy/returnTheSumOfTwoNumbers.js
@@ -0,0 +1,18 @@
+/*
+Create a function that takes two numbers as arguments and return their sum.
+
+Notes
+- Don't forget to return the result.
+- If you get stuck on a challenge, find help in the Resources tab.
+- If you're really stuck, unlock solutions in the Solutions tab.
+
+function addition(a, b) {
+	
+}
+
+Published by formateur
+*/
+
+function addition(a, b) {
+	return a + b;
+};
