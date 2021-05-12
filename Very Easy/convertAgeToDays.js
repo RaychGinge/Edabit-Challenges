a5d982e3d59e375a47926c25e9660ccef7ae0857
RaychGinge <60284593+RaychGinge@users.noreply.github.com>
2021-05-12 22:03:18 +0100

    Create convertAgeToDays.js
    create a function that takes in years, and returns the age is days

---
+++ Very Easy/convertAgeToDays.js
@@ -0,0 +1,15 @@
+/* 
+Create a function that takes in years and returns the age in days
+
+Notes:
+- Use 365 days as the length of a year for this challenge
+- Ignore leap years and days between last birthday and now
+- Expect only positive integer ouputs
+
+function calcAge(age) |{
+
+}
+
+published by medyah99
+*/
+
